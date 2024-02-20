742f058ebbba5f20988ee75d12a5bb0404480741
Oscar Omar Flores Sotomayor <oscaromarfloressotomayor@gmail.com>
2024-02-19 18:44:01 -0600

    Agrego carpeta para entidades y primera entidad llamada Product

---
+++ Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace API_ECOMMERCE
+{
+     public class Product
+     {
+        public int Id { get; set; } //EFC ya entiende qu es el id solo por el nombre
+
+        public string Name { get; set; }
+
+     }
+}
