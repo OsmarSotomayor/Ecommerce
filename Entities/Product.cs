a39f5f16b1063e7dd8dafdbff4ba04ffa350588d
Oscar Omar Flores Sotomayor <oscaromarfloressotomayor@gmail.com>
2024-02-19 19:28:19 -0600

    Agrego referencias a EFC y clase DbContext

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -3,8 +3,6 @@ namespace API_ECOMMERCE
      public class Product
      {
         public int Id { get; set; } //EFC ya entiende qu es el id solo por el nombre
-
         public string Name { get; set; }
-
      }
 }
