21e4e95c3e2c6be74aa8bb8795febdb252e59197
Oscar Omar Flores Sotomayor <oscaromarfloressotomayor@gmail.com>
2024-03-09 12:06:28 -0600

    Agrego propiedades de navegacion y early loading

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -13,18 +13,26 @@ namespace Infrastructure.Data
         }
 
         public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
-        {
+        {          
             return await context.ProductBrands.ToListAsync();
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await context.Products.FindAsync(id);
-        }
+            return await context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .FirstOrDefaultAsync(p=> p.Id == id);
+        }   
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await context.Products.ToListAsync();
+            //.ToListAsync() es el punto donde se envia la solicitud o consulta a la bbd
+            //Es el punto donde se hace la solicitud y se nos devuelven los datos
+            return await context.Products
+            .Include(p => p.ProductType)
+            .Include(p=> p.ProductBrand)
+            .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
@@ -32,5 +40,4 @@ namespace Infrastructure.Data
             return await context.ProductTypes.ToArrayAsync();
         }
     }
-
 }
